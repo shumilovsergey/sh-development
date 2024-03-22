53b96133e13a3f64246dd25e884da9840bc09e96
shumilov <wumilovsergey@gmail.com>
2024-03-22 19:23:22 +0300

    up

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Job
+from .models import Career
+from .models import Bio
+from .models import Skills
+from .models import Feedback
+from .models import Projects
 
-# Register your models here.
+admin.site.register(Job)
+admin.site.register(Career)
+admin.site.register(Bio)
+admin.site.register(Skills)
+admin.site.register(Feedback)
+admin.site.register(Projects)
